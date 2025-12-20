a9548951f761fe28c1f438248ecd3fa734ba12ee
Rahul <rahul.dsg@gmail.com>
2025-12-20 19:24:36 +0530

    feat: add server setup with Express, MongoDB connection, and environment configuration

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # money-manager-be
+
+# Setup
+
+# Setup - Mongo DB
+    - create a new account
+    - create a new cluster
+    - create a new user
+    - copy the connection
+    - whitelist IP Address
+
+
+# Connect mongo DB from node application
+    - install mongoose package - npm i mongoose
+    - create a new .env file - npm i dotenv
+
+
+# HTTP Methods and status code
+
+# Routing
+
+# Express Validation
+
+# Middleware
--- src/server.js
+++ src/server.js
@@ -1,17 +1,49 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
-const port = 3000;
+class Server {
+    app = express();
+    port = null;
+    constructor() {
+        this.setConfig();
+        this.connectDB();
+        this.setRoutes();
+    }
 
-app.get('/about', (req, res) => {
-  res.send('Hello, this is the about page.');
-});
+    setConfig() {
+        dotenv.config({ path: './.env' });
+        this.port = process.env.PORT;
+        this.app.use(express.json());
+    }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    connectDB() {
+        mongoose.connect(process.env.MONGO_DB_URI)
+        .then(() => {
+            console.log('Database Connected successfully!');
+            this.start();
+        });
+    }
+
+    start() {
+        this.app.listen(this.port, () => {
+            console.log(`Server is running at http://localhost:${this.port}`);
+        });
+    }
+
+    setRoutes() {
+        this.app.get('/', (req, res) => {
+            res.json(
+                {
+                    name: "Money Manager API test",
+                    version: "1.0.0",
+                    description: "This is a simple money manager API built with Express.js",
+                }
+            ).status(200);
+        });
 
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+    }
+}
+
+new Server();
